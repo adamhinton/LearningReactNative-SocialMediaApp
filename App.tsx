34dbeb627718b49bc3507b6f47d9f6a68dce0057
adamhinton <adam.d.hinton@gmail.com>
2024-05-04 13:03:00 -0400

    function for updating font familt

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
 /* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import {SafeAreaView, Text, View} from 'react-native';
+import {SafeAreaView, Text} from 'react-native';
+import {getFontFamily} from './assets/fonts/helper';
 
 const App = () => {
   return (
     <SafeAreaView>
-      <Text
-        style={{fontSize: 50, fontFamily: 'Inter-Black', fontWeight: '500'}}>
+      <Text style={{fontSize: 50, fontFamily: getFontFamily('Inter', '100')}}>
         Hello World!
       </Text>
     </SafeAreaView>
