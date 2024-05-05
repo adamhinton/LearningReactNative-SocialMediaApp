86eb1a27aad4c76385d4766358c7c70e5dbf704e
adamhinton <adam.d.hinton@gmail.com>
2024-05-05 12:32:39 -0400

    new globalStyles

--- App.tsx
+++ App.tsx
@@ -1,23 +1,27 @@
 /* eslint-disable react-native/no-inline-styles */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import React from 'react';
-import {SafeAreaView, View} from 'react-native';
+import {SafeAreaView, TouchableOpacity, View} from 'react-native';
 import {getFontFamily} from './assets/fonts/helper';
 import Title from './components/Title/Title';
 import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
 import {faEnvelope} from '@fortawesome/free-regular-svg-icons';
+import globalStyle from './assets/styles/globalStyles';
 
 const App = () => {
   return (
     <SafeAreaView>
-      <View
-        style={{
-          marginLeft: 27,
-          marginRight: 17,
-          marginTop: 30,
-        }}>
+      <View style={globalStyle.header}>
         <Title title={"Let's explore"} />
-        <FontAwesomeIcon icon={faEnvelope} />
+        <TouchableOpacity
+          style={{
+            padding: 14,
+            // I changed this to make it easier to see
+            backgroundColor: '#bfd2e4',
+            borderRadius: 100,
+          }}>
+          <FontAwesomeIcon icon={faEnvelope} color="898DAE" />
+        </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
---
+++ assets/styles/globalStyles.ts
@@ -0,0 +1,14 @@
+import {StyleSheet} from 'react-native';
+
+const globalStyle = StyleSheet.create({
+  header: {
+    marginLeft: 27,
+    marginRight: 17,
+    marginTop: 30,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+});
+
+export default globalStyle;
