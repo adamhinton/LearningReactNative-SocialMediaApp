e5df78a5b73b63f9ccbcf544e188778a781f0f8a
adamhinton <adam.d.hinton@gmail.com>
2024-05-05 12:00:54 -0400

    basic Title styling

--- App.tsx
+++ App.tsx
@@ -1,17 +1,14 @@
-/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 import {SafeAreaView, Text} from 'react-native';
 import {getFontFamily} from './assets/fonts/helper';
+import Title from './components/Title/Title';
 
 const App = () => {
   return (
     <SafeAreaView>
-      <Text style={{fontSize: 50, fontFamily: getFontFamily('Inter', '100')}}>
-        Hello World!
-      </Text>
+      <Title title={"Let's explore"} />
     </SafeAreaView>
   );
 };
 
 export default App;
- 
--- components/Title/Title.tsx
+++ components/Title/Title.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import style from './style';
+import {Text} from 'react-native';
 
-const Title = () => {};
+type Props = {
+  title: string;
+};
+
+const Title = (props: Props) => {
+  return <Text style={style.title}>{props.title}</Text>;
+};
 
 export default Title;
-
-import style from 'styles'
