6d4c5e2dad5df2075b35bdfcb5a7989e8bbf9e1a
adamhinton <adam.d.hinton@gmail.com>
2024-05-05 12:21:38 -0400

    add icon

--- App.tsx
+++ App.tsx
@@ -1,13 +1,24 @@
+/* eslint-disable react-native/no-inline-styles */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import React from 'react';
 import {SafeAreaView, View} from 'react-native';
 import {getFontFamily} from './assets/fonts/helper';
 import Title from './components/Title/Title';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import {faEnvelope} from '@fortawesome/free-regular-svg-icons';
 
 const App = () => {
   return (
     <SafeAreaView>
-      <Title title={"Let's explore"} />
+      <View
+        style={{
+          marginLeft: 27,
+          marginRight: 17,
+          marginTop: 30,
+        }}>
+        <Title title={"Let's explore"} />
+        <FontAwesomeIcon icon={faEnvelope} />
+      </View>
     </SafeAreaView>
   );
 };
