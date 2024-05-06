029b488a645ba882770a9cfdf824d99d4ab5c591
adamhinton <adam.d.hinton@gmail.com>
2024-05-06 10:57:46 -0400

    Starting on profile image component

--- components/UserStory/UserStory.tsx
+++ components/UserStory/UserStory.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import {UserStoryType} from '../../App';
 import {Image, Text, View} from 'react-native';
+import style from './style';
 
 const UserStory = (props: UserStoryType) => {
   return (
-    <View>
-      <Image source={props.profileImage} />
-      <Text>{props.firstName}</Text>
+    <View style={style.storyContainer}>
+      <View style={style.userImageContainer}>
+        <Image source={props.profileImage} style={style.image} />
+      </View>
+      <Text style={style.firstName}>{props.firstName}</Text>
     </View>
   );
 };
