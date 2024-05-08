63f9bbe4fc082346a3af30577b6f0ec49b5310c3
adamhinton <adam.d.hinton@gmail.com>
2024-05-08 15:04:41 -0400

    import default post img

--- components/UserPost/UserPost.tsx
+++ components/UserPost/UserPost.tsx
@@ -1,1 +1,17 @@
 import React from 'react';
+
+type Props = {
+  firstName: string;
+  lastName: string;
+  location?: string;
+  image: any;
+  likes: number;
+  comments: number;
+  bookmarks: number;
+};
+
+const UserPost = (props: Props) => {
+  return null;
+};
+
+export default UserPost;
