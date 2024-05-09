8828d99555bf99aa1fbc41a1e85353df593ab776
adamhinton <adam.d.hinton@gmail.com>
2024-05-08 21:55:31 -0400

    UserPost styling coming togehter

--- components/UserPost/UserPost.tsx
+++ components/UserPost/UserPost.tsx
@@ -5,6 +5,7 @@ import {Text, View} from 'react-native';
 import UserProfileImage from '../UserProfileImage/UserProfileImage';
 import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
 import {faEllipsisH} from '@fortawesome/free-solid-svg-icons';
+import style from './style';
 
 type Props = {
   firstName: string;
@@ -30,26 +31,22 @@ const UserPost = (props: Props) => {
   } = props;
 
   return (
-    <View
-      style={{
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-      }}>
-      <View>
-        <UserProfileImage
-          profileImage={props.profileImage}
-          imageDimensions={48}
-        />
-        <View
-          style={{
-            justifyContent: 'center',
-            marginLeft: 10,
-          }}>
-          <Text>
-            {firstName} {lastName}
-          </Text>
-          {location && <Text>{location}</Text>}
+    <View style={style.user}>
+      <View
+        style={{
+          flexDirection: 'row',
+        }}>
+        <View style={style.userContainer}>
+          <UserProfileImage
+            profileImage={props.profileImage}
+            imageDimensions={48}
+          />
+          <View style={style.userTextContainer}>
+            <Text>
+              {firstName} {lastName}
+            </Text>
+            {location && <Text>{location}</Text>}
+          </View>
         </View>
       </View>
       <FontAwesomeIcon icon={faEllipsisH} />
--- components/UserPost/style.ts
+++ components/UserPost/style.ts
@@ -1,3 +1,18 @@
 import {StyleSheet} from 'react-native';
 
-const style = StyleSheet.create({});
+const style = StyleSheet.create({
+  userContainer: {
+    flexDirection: 'row',
+  },
+  userTextContainer: {
+    justifyContent: 'center',
+    marginLeft: 10,
+  },
+  user: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+});
+
+export default style;
