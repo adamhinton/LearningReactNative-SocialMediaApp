27e96f0636e0ac38146f89dee912f504f7fe2086
adamhinton <adam.d.hinton@gmail.com>
2024-05-18 23:52:11 -0400

    Install drawernavigator stuff

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,5 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  // per docs, make sure react-native-reanimated/lugin is listed last
+  plugins: ['react-native-reanimated/plugin'],
 };
