36519e5d406c2fe4a856d0a568ce9f98bea86b14
adamhinton <adam.d.hinton@gmail.com>
2024-05-19 09:24:57 -0400

    clear unneeded stuff

--- screens/Profile/Profile.tsx
+++ screens/Profile/Profile.tsx
@@ -1,23 +1,12 @@
-/* eslint-disable react-native/no-inline-styles */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React from 'react';
-import {Text} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import globalStyle from '../../assets/styles/globalStyle';
-
-import {TouchableOpacity} from 'react-native-gesture-handler';
 
 // @ts-ignore
 const Profile = ({navigation}) => {
   return (
-    <SafeAreaView style={[globalStyle.backgroundWhite, globalStyle.flex]}>
-      <TouchableOpacity
-        onPress={() => {
-          navigation.goBack();
-        }}>
-        <Text style={{color: 'blue'}}>Go back</Text>
-      </TouchableOpacity>
-      <Text>Welcome to profile page</Text>
-    </SafeAreaView>
+    <SafeAreaView style={[globalStyle.backgroundWhite, globalStyle.flex]} />
   );
 };
 
