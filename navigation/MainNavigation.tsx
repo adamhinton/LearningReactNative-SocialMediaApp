a56a69d29edfa4c82608c75422752f557c107a1a
adamhinton <adam.d.hinton@gmail.com>
2024-05-20 00:47:20 -0400

    get createMaterialTopTabNavigator in Profile

--- navigation/MainNavigation.tsx
+++ navigation/MainNavigation.tsx
@@ -1,12 +1,15 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import {Routes} from './Routes';
 import Home from '../screens/Home/Home';
 import Profile from '../screens/Profile/Profile';
 import {createDrawerNavigator} from '@react-navigation/drawer';
+import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
 
 const Stack = createStackNavigator();
 const Drawer = createDrawerNavigator();
+const ProfileTabs = createMaterialTopTabNavigator();
 
 const MainMenuNavigation = () => {
   return (
